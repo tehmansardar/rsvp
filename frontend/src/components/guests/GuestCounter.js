ead72a4d1dcdc6abb9ccb0481b02574d2b3eed0a
Tehman Sardar <tehmansardar@gmail.com>
2021-01-05 05:52:20 +0500

    Guest Counter Setup

--- frontend/src/components/guests/GuestCounter.js
+++ frontend/src/components/guests/GuestCounter.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import GuestContext from '../../context/guestContext/GuestContext';
 
 const GuestCounter = () => {
+	const { guests } = useContext(GuestContext);
+
+	const totalInvited = guests.length;
+	const attending = guests.filter((guest) => guest.isconfirmed);
+	const totalAttending = attending.length;
+	const invitedByDiet = (type) =>
+		guests.filter((guest) => guest.diet === type).length;
+	const attendingByDiet = (type) =>
+		attending.filter((guest) => guest.diet === type).length;
 	return (
 		<div>
 			<table>
@@ -12,23 +22,23 @@ const GuestCounter = () => {
 					</tr>
 					<tr>
 						<th>Non-Veg</th>
-						<td>10</td>
-						<td>5</td>
+						<td>{invitedByDiet('Non-Veg')}</td>
+						<td>{attendingByDiet('Non-Veg')}</td>
 					</tr>
 					<tr>
 						<th>Vegan</th>
-						<td>10</td>
-						<td>5</td>
+						<td>{invitedByDiet('Veg')}</td>
+						<td>{attendingByDiet('Veg')}</td>
 					</tr>
 					<tr>
 						<th>Pescetarians</th>
-						<td>10</td>
-						<td>5</td>
+						<td>{invitedByDiet('Pasc')}</td>
+						<td>{attendingByDiet('Pasc')}</td>
 					</tr>
 					<tr>
 						<th>Total</th>
-						<td>10</td>
-						<td>5</td>
+						<td>{totalInvited}</td>
+						<td>{totalAttending}</td>
 					</tr>
 				</tbody>
 			</table>
