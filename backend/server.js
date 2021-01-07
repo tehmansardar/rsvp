2ee509c7a7f1ca1e8a793917a597e659de78fbaf
Tehman Sardar <tehmansardar@gmail.com>
2021-01-08 02:50:15 +0500

    NodeJs Setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+// importing
+import express from 'express';
+
+// app config
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+
+// DB config
+
+// api routes
+
+// listen
+app.listen(port, () => console.log(`Running on Port ${port}`));
