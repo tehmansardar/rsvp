45996144832211eb2dd6b2d6287fa41651750b1e
Tehman Sardar <tehmansardar@gmail.com>
2021-01-08 03:20:46 +0500

    Routes Initialized

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 // importing
 import express from 'express';
 import connectDB from './config/db.js';
+import router from './routes/register.js';
 // app config
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
+app.use(express.json({ extended: true }));
+app.use('/register', router);
 
 // DB config
 connectDB();
