4e5deecc7c079ea89106a7f57bc07da89320d139
Tehman Sardar <tehmansardar@gmail.com>
2021-01-09 03:13:49 +0500

    applying validation using express-validator

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,8 +1,27 @@
 import express from 'express';
-const router = express.Router();
+import { check, validationResult } from 'express-validator';
 
-router.post('/', (req, res) => {
-	res.send('User Registered');
-});
+//config
+const router = express.Router();
+// http://localhost:5000/register
+router.post(
+	'/',
+	[
+		check('name', 'Please provide a name').not().isEmpty(),
+		check('email', 'Please provide a valid email').isEmail(),
+		check('password', 'Please provide 6 character long password').isLength({
+			min: 6,
+		}),
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ error: errors.array() });
+		}
+		res.send('Success');
+		const { name, email, password } = req.body;
+	}
+);
 
 export default router;
