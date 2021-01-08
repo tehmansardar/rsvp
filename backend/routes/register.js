1cac34f66ffc6d6de6d0c1598f924353e9170412
Tehman Sardar <tehmansardar@gmail.com>
2021-01-09 04:13:50 +0500

    Generate jsonwebtoken

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { check, validationResult } from 'express-validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 // User model
 import User from '../models/User.js';
@@ -17,14 +19,49 @@ router.post(
 			min: 6,
 		}),
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req);
 
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ error: errors.array() });
 		}
-		res.send('Success');
 		const { name, email, password } = req.body;
+		try {
+			let user = await User.findOne({ email });
+			if (user) {
+				return res.status(400).json({ msg: 'User already exists' });
+			}
+			user = new User({
+				name,
+				email,
+				password,
+			});
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(password, salt);
+
+			await user.save();
+
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(
+				payload,
+				process.env.SECRET,
+				{
+					expiresIn: 3600,
+				},
+				(err, token) => {
+					if (err) throw err;
+					res.send({ token });
+				}
+			);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
 	}
 );
 
