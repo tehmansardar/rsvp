08f71999f831d628de16e08bdbbd7b23bd4d05eb
Tehman Sardar <tehmansardar@gmail.com>
2021-01-11 05:26:21 +0500

    Post the Guest

--- backend/routes/guests.js
+++ backend/routes/guests.js
@@ -1,5 +1,6 @@
 // importing
 import express from 'express';
+import { check, validationResult } from 'express-validator';
 
 // middleware
 import auth from '../middleware/auth.js';
@@ -11,6 +12,10 @@ const router = express.Router();
 import Guest from '../models/Guest.js';
 
 // api routes
+
+// @route Get /guests
+// @des Get guests
+// @access Private
 router.get('/', auth, async (req, res) => {
 	try {
 		const guests = await Guest.find({ user: req.user.id });
@@ -20,5 +25,40 @@ router.get('/', auth, async (req, res) => {
 		res.status(500).send('Server Error');
 	}
 });
+
+// @route POST /guests
+// @des Add new guest
+// @access Private
+router.post(
+	'/',
+	auth,
+	[
+		check('name', 'Please Place the name').not().isEmpty(),
+		check('phone', 'Please provide the phone').not().isEmpty(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { name, phone, diet, isconfirmed } = req.body;
+
+		try {
+			const newGuest = await new Guest({
+				user: req.user.id,
+				name,
+				phone,
+				diet,
+				isconfirmed,
+			});
+			const guest = await newGuest.save();
+			res.json(guest);
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).send('server Error');
+		}
+	}
+);
 
 export default router;
